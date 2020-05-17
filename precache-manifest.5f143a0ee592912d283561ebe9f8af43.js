4edc326059240559321a14babfcbb2e2bc6687d8
ibrahimtaj <taj_it@yahoo.com>
2020-05-17 19:52:29 +0530

    Updates

---
+++ precache-manifest.5f143a0ee592912d283561ebe9f8af43.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332445ce1619e93d483037db96989419",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e4daa3fabfbca6ecaa01",
+    "url": "/my-app/static/css/main.614f60cb.chunk.css"
+  },
+  {
+    "revision": "b72225362d2ccbebafac",
+    "url": "/my-app/static/js/2.829509fd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/my-app/static/js/2.829509fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4daa3fabfbca6ecaa01",
+    "url": "/my-app/static/js/main.996944f2.chunk.js"
+  },
+  {
+    "revision": "1b58fcb5ce8d26145ee7",
+    "url": "/my-app/static/js/runtime-main.8b909c6e.js"
+  },
+  {
+    "revision": "a776da443f4a5c5689f592a9b630b270",
+    "url": "/my-app/static/media/1.a776da44.jpg"
+  },
+  {
+    "revision": "8f16a9118801c4b76188a6d4ebf5a072",
+    "url": "/my-app/static/media/2.8f16a911.jpg"
+  },
+  {
+    "revision": "513fcc5ca24db21f2a54c36b92496149",
+    "url": "/my-app/static/media/3.513fcc5c.jpg"
+  },
+  {
+    "revision": "961695bab0b439f7956d66be590ce49d",
+    "url": "/my-app/static/media/4.961695ba.jpg"
+  },
+  {
+    "revision": "2646ac66c2946bfc807c185fd1b0e195",
+    "url": "/my-app/static/media/5.2646ac66.jpg"
+  },
+  {
+    "revision": "0123181a4e8be8bffe74378a3173df79",
+    "url": "/my-app/static/media/6.0123181a.jpg"
+  },
+  {
+    "revision": "603e31c448b7aee327258713870c264e",
+    "url": "/my-app/static/media/7.603e31c4.jpg"
+  },
+  {
+    "revision": "91f8c6dd916ad112af36e84124444e31",
+    "url": "/my-app/static/media/8.91f8c6dd.jpg"
+  },
+  {
+    "revision": "9dc6a320cf9ff1dac3c36cce56918c61",
+    "url": "/my-app/static/media/9.9dc6a320.jpg"
+  }
+]);
